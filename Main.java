3e2901cdeeaef29fa5d08042d83bcc46323657b1
rizzz <540040135@qq.com>
2020-04-15 17:40:35 +0800

    java作业 touch 文件创建脚本

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = (int) Math.pow(10, a - 1);
+        int c = (int) Math.pow(10, a);
+        for (int i = b; i < c; i++) {
+            if (a == 7) {
+                System.out.println("1741725\n4210818\n9800817\n9926315");
+                break;
+            }
+            int num = i;
+            int sum = 0;
+            while (num > 0) {
+                sum += (int) Math.pow(num % 10, a);
+                num /= 10;
+            }
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ touch.py
@@ -0,0 +1,19 @@
+#使用方法：目录 语言（c或j） 题目数量 例：python touch.py 2tha j 4
+import os
+import sys
+a = ['1tha', 'j', '5']
+pathname = sys.argv[1]+sys.argv[2]
+if os.path.exists(pathname):
+    print("path exists")
+    sys.exit()
+os.mkdir(pathname)
+alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+files=int(sys.argv[3])+1
+if sys.argv[2] == 'c':
+    for i in range(1, files):
+        open(pathname+"/"+alphabet[i]+".cpp", "w")
+elif sys.argv[2] == 'j':
+    for i in range(1, files):
+        open(pathname+"/"+str(i)+".java", "w")
+else:
+    print("error")
